7d5832ca77c219881c33e55553b27809ed836e3e
Ppedro-leal <pedroh.leal18@gmail.com>
2025-12-02 11:51:33 -0300

    Fix: renomeação no tipo de arquivo de js para jsx

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Providers } from "../providers";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Bookly | Sua Rede de Livros",
+  description: "Plataforma de compartilhamento e empréstimo de livros.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br" className={inter.className}>
+      <body className="flex flex-col min-h-screen bg-gray-50">
+        <Providers>
+          <main className="grow">{children}</main>
+        </Providers>
+      </body>
+    </html>
+  );
+}
